c17169232aef69bf541cc9bf441cbef97237000c
Lucas <jlabbude@hotmail.com>
2024-11-16 15:37:21 -0300

    pain and suffering

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 #![allow(deprecated, clippy::redundant_closure)]
-use chrono::{DateTime, Local};
-use rand::Rng;
-use serde_json::json;
+use chrono::{DateTime, Local, TimeZone};
+use serde_json::{json, Value};
 use std::collections::VecDeque;
+use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsValue;
+use web_sys::Element;
+use web_sys::HtmlCanvasElement;
 use yew::prelude::*;
 use yew_hooks::use_interval;
-use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(module = "/src/package.js")]
 extern "C" {
@@ -27,82 +28,122 @@ struct TdsGraphProps {
     history: VecDeque<TdsDataPoint>,
 }
 
+fn serialize_tds_json(val: &Value) -> TdsDataPoint {
+    TdsDataPoint {
+        value: val["tds_ppm"].as_f64().unwrap(),
+        timestamp: Local
+            .timestamp_opt(val["timestamp"].as_i64().unwrap(), 0)
+            .earliest()
+            .unwrap(),
+    }
+}
+
 pub fn is_chart_instances_empty() -> bool {
-    js_sys::Object::keys(CHART_INSTANCES.dyn_ref::<js_sys::Object>()
-        .expect("CHART_INSTANCES is not an object"))
-        .length() == 0
+    js_sys::Object::keys(
+        CHART_INSTANCES
+            .dyn_ref::<js_sys::Object>()
+            .expect("CHART_INSTANCES is not an object"),
+    )
+    .length()
+        == 0
 }
 
-fn fetch_data() -> f64 {
-    let data: f64 = rand::thread_rng().gen_range(0.0..1000.0);
-    log::info!("DATA {data}");
-    data
+#[hook]
+fn use_fetch_latest_data() -> Option<TdsDataPoint> {
+    let latest_data = use_state_eq(|| None);
+
+    {
+        let latest_data = latest_data.clone();
+        wasm_bindgen_futures::spawn_local(async move {
+            if let Ok(response) = reqwest::get("http://localhost:8000/last_message").await {
+                if let Ok(json_data) = response.json::<Value>().await {
+                    let new_data = serialize_tds_json(&json_data);
+                    latest_data.set(Some(new_data));
+                }
+            } else {
+                latest_data.set(None);
+            }
+        });
+    }
+
+    *latest_data
 }
 
-fn tds_history(old_history: &VecDeque<TdsDataPoint>, current_value: f64) -> VecDeque<TdsDataPoint> {
+fn tds_history(
+    old_history: &VecDeque<TdsDataPoint>,
+    current_value: TdsDataPoint,
+) -> VecDeque<TdsDataPoint> {
     let mut new_history = old_history.clone();
     if new_history.len() > 60 {
         new_history.pop_front();
     }
-    new_history.push_back(TdsDataPoint {
-        value: current_value,
-        timestamp: Local::now(),
-    });
+    new_history.push_back(current_value);
     new_history.clone()
+}
+
+/// `yew_hooks::use_interval` misbehaves if i dont do this
+fn refresh(
+    old_history: &VecDeque<TdsDataPoint>,
+) -> VecDeque<TdsDataPoint> {
+    old_history.clone()
 }
 
 #[function_component]
 fn Graph(props: &TdsGraphProps) -> Html {
-    use wasm_bindgen::JsValue;
-    use web_sys::HtmlCanvasElement;
-    
-
-    let canvas_ref = use_node_ref(); // Reference to the canvas element
+    let canvas_ref = use_node_ref();
 
     {
         let canvas_ref = canvas_ref.clone();
         let history = props.history.clone();
 
-        use_effect_with(
-            props.history.clone(), // Re-run effect when history updates
-            move |_| {
-                // Prepare the data for the chart
-                let labels: Vec<String> = history
-                    .iter()
-                    .map(|point| point.timestamp.format("%H:%M:%S").to_string())
-                    .collect();
-                let data: Vec<f64> = history.iter().map(|point| point.value).collect();
+        use_effect_with(props.history.clone(), move |_| {
+            let labels: Vec<String> = history
+                .iter()
+                .map(|point| point.timestamp.format("%H:%M:%S").to_string())
+                .collect();
+            let data: Vec<f64> = history.iter().map(|point| point.value).collect();
 
-                let chart_data = json!({
-                    "labels": labels,
-                    "datasets": [{
-                        "label": "TDS (ppm)",
-                        "data": data,
-                        "fill": false,
-                        "borderColor": "rgba(75, 192, 192, 1)",
-                        "tension": 0.1
-                    }]
-                });
+            let chart_data = json!({
+                "labels": labels,
+                "datasets": [{
+                    "label": "TDS (ppm)",
+                    "data": data,
+                    "fill": false,
+                    "borderColor": "rgba(75, 192, 192, 1)",
+                    "tension": 0.1
+                }]
+            });
 
-                let options = json!({
-                    "scales": {
-                        "y": {
-                            "beginAtZero": true
-                        }
-                    }
-                });
-
-                // Get the canvas element
-                if let Some(canvas) = canvas_ref.cast::<HtmlCanvasElement>() {
-                    match is_chart_instances_empty() {
-                        true => { createChart(&canvas.id(), JsValue::from_serde(&chart_data).unwrap(), JsValue::from_serde(&options).unwrap()); },
-                        false => updateChart(&canvas.id(), JsValue::from_serde(&chart_data).unwrap()),
+            let options = json!({
+                "scales": {
+                    "y": {
+                        "beginAtZero": true
                     }
                 }
+            });
 
-                || ()
-            },
-        );
+            if let Some(canvas) = canvas_ref.cast::<HtmlCanvasElement>() {
+                if let Some(id) = canvas
+                    .dyn_ref::<Element>()
+                    .and_then(|el| el.get_attribute("id"))
+                {
+                    match is_chart_instances_empty() {
+                        true => {
+                            createChart(
+                                &id,
+                                JsValue::from_serde(&chart_data).unwrap(),
+                                JsValue::from_serde(&options).unwrap(),
+                            );
+                        }
+                        false => {
+                            updateChart(&id, JsValue::from_serde(&chart_data).unwrap());
+                        }
+                    }
+                }
+            }
+
+            || ()
+        });
     }
 
     html! {
@@ -110,33 +151,32 @@ fn Graph(props: &TdsGraphProps) -> Html {
     }
 }
 
-
 #[function_component]
 fn App() -> Html {
-    let data = use_state(|| fetch_data());
-    let mut deque = VecDeque::with_capacity(60);
-    (1..60).into_iter().for_each(|_| {
-        deque.push_back(TdsDataPoint {
-            value: fetch_data(),
-            timestamp: Local::now(),
-        });
-    });
-    let history = use_state(|| deque);
+    let history = use_state(|| VecDeque::<TdsDataPoint>::with_capacity(60));
+    let data_op = use_fetch_latest_data();
     {
         let history = history.clone();
-        let data = data.clone();
         use_interval(
             move || {
-                history.set(tds_history(&history, *data));
-                data.set(fetch_data());
+                if let Some(data) = &data_op {
+                    let normalized_timestamp = data.timestamp.timestamp();
+                    if history.back().map_or(true, |front| front.timestamp.timestamp() != normalized_timestamp) {
+                        history.set(tds_history(&history, data.clone()));
+                    } else {
+                        history.set(refresh(&history));
+                    }
+                }
             },
-            60*1000,
+            1000
         );
     }
 
+    let data = data_op.as_ref().map_or(0.0, |data| data.value);
     let get_quality_level = |tds: f64| -> (&'static str, &'static str) {
         match tds {
-            0.0..100.0 => ("Excelente", "#4CAF50"),
+            0.0 => ("Desconectado", "#000000"),
+            1.0..100.0 => ("Excelente", "#4CAF50"),
             100.0..200.0 => ("Bom", "#8BC34A"),
             200.0..300.0 => ("Aceitável", "#FFC107"),
             300.0..400.0 => ("Ruim", "#FF9800"),
@@ -145,7 +185,7 @@ fn App() -> Html {
         }
     };
 
-    let (quality, color) = get_quality_level(*data);
+    let (quality, color) = get_quality_level(data);
 
     html! {
         <div class="container">
@@ -153,7 +193,7 @@ fn App() -> Html {
                 <div class="card">
                     <h2>{"TDS Atual"}</h2>
                     <div class="value">
-                        {format!("{:.1} ppm", *data)}
+                        {format!("{:.1} ppm", data)}
                     </div>
                 </div>
 
